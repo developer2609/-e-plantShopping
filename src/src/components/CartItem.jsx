c2551b71709983419687c11eeb041b4ff4209e38
Dostonbek <110821315+developer2609@users.noreply.github.com>
2026-01-30 17:31:27 +0500

    Add CartItem component for shopping cart functionality

---
+++ src/src/components/CartItem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeItem, updateQuantity } from "../redux/CartSlice";
+
+const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="cart-item">
+      <h4>{item.name}</h4>
+      <p>Quantity: {item.quantity}</p>
+      <button
+        onClick={() =>
+          dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1 }))
+        }
+      >
+        +
+      </button>
+      <button
+        onClick={() =>
+          dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 }))
+        }
+      >
+        -
+      </button>
+      <button onClick={() => dispatch(removeItem(item.id))}>Remove</button>
+    </div>
+  );
+};
+
+export default CartItem;
